3a11a96ab3e4b5ae36fa61349308e7bc53bd9ce4
hamicodes <hamza.codes@outlook.com>
2023-04-21 01:22:16 +0500

    test claim func

--- contracts/IDO.sol
+++ contracts/IDO.sol
@@ -66,7 +66,7 @@ contract IDO {
     }
 
     function claim() external notHalted {
-        require(block.timestamp >= claimStart);
+        require(block.timestamp >= claimStart, "!time");
 
         uint _claims = s_claims[msg.sender];
 
--- test/IDO.t.sol
+++ test/IDO.t.sol
@@ -6,6 +6,8 @@ import {ERC20} from "../mocks/MockERC20.sol";
 import {console, stdStorage, StdStorage} from "forge-std/Test.sol";
 
 contract Test_IDO is CommonBase {
+    using stdStorage for StdStorage;
+
     function setUp() public virtual override {
         super.setUp();
         vm.startPrank(owner);
@@ -21,19 +23,46 @@ contract Test_IDO is CommonBase {
         vm.stopPrank();
     }
 
-    function test_claimAmount(uint _amount) public {
-        _claimAmount(_amount);
-    }
-
-    function test_amounts(uint _amount) public {
-        vm.assume(_amount <= ido.investMax() && _amount >= ido.investMin());
-
+    function test_claim() public {
         approveIdo(token.decimals());
 
         skip(ido.saleStart());
 
-        vm.prank(dev);
-        ido.invest{value: _amount}();
+        vm.startPrank(dev);
+        ido.invest{value: 1 ether}();
+
+        skip(ido.saleEnd());
+
+        vm.expectRevert();
+        ido.claim();
+
+        skip(ido.claimStart());
+
+        uint _preBal = token.balanceOf(dev);
+        uint _preClaims = ido.s_claims(dev);
+
+        ido.claim();
+
+        uint _postBal = token.balanceOf(dev);
+        uint _postClaims = ido.s_claims(dev);
+
+        assertEq(_postClaims, 0);
+        assertEq(_postBal, _preClaims + _preBal);
+    }
+
+    function test_claimAmount(uint _amount) public {
+        _claimAmount(_amount);
+    }
+
+    function test_goalReached() public {
+        skip(ido.saleStart());
+
+        stdstore.target(address(ido)).sig("s_raised()").checked_write(
+            ido.hardcap()
+        );
+
+        vm.expectRevert();
+        ido.invest{value: 1 ether}();
     }
 
     function test_saleEnded() public {
